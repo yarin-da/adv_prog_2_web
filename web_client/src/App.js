2fcd8e3b6590c916607df3b5203f85bab34dfb4b
yarin-da <dado.yarin@gmail.com>
2021-04-29 19:15:11 +0300

    changed the default page to display basic empty panels

---
+++ web_client/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="MainApp">
+      <div className="SidePanel">
+        <header className="Title">Model Load Panel</header>
+      </div>
+      <div className="GraphPanel">
+        <header className="Title">Graph Panel</header>
+      </div>
+      <div className="TablePanel">
+        <header className="Title">Table Panel</header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
